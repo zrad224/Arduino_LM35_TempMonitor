05b29268ba5757c91303fea13cf2db7becb294e8
zrad224 <ohozxsw@gmail.com>
2026-04-15 03:00:54 +0700

    feat: ho tro doc 2 kenh LM35 (A0, A1)

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,5 +1,6 @@
-int adcValue;
-float nhietDo;
+int adcValues[2];
+float nhietDo[2];
+char chuoi[30];
 
 void setup() {
   Serial.begin(9600);
@@ -7,8 +8,17 @@ void setup() {
 }
 
 void loop() {
-  adcValue = analogRead(A0);
-  nhietDo = (adcValue * 500.0) / 1023.0;
-  Serial.println(nhietDo, 1);
-  delay(1000);
+  // Doc kenh A0
+  adcValues[0] = analogRead(A0);
+  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
+
+  // Doc kenh A1
+  adcValues[1] = analogRead(A1);
+  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
+
+  // Gui du lieu dang CSV
+  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
+  Serial.print(chuoi);
+
+  delay(100);
 }
